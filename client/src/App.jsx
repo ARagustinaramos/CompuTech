45579c4956e04e6be5db09b455928f4f7aa39610
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-05-21 19:15:23 -0300

    Back listo + redux configurado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,11 @@ import './App.css'
 import SearchBar from './components/searchBar/SearchBar'
 import Home from './views/home/Home'
 import Detail from './views/detail/Detail'
-<<<<<<< HEAD
+
 //import Create from './views/create/Create'
 //import Landing from "./views/landing/Landing"
-=======
->>>>>>> d3f127ba640bf6e34a3dac4b5bc6b157bab64375
+
+
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Form from './components/form/Form'
--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,116 +1,120 @@
-import axios from "axios";
-import { GET_DETAIL, GET_POKEMONS, GET_BY_NAME, GET_TYPES,FILTERDBAPI,FILTER_TYPE,ORDER_NAME,ORDER_ATTACK,CLEAN_DETAIL } from "./types";
+import axios from 'axios';
 
-export const getPokemons = () => {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios.get("http://localhost:3001/pokemons");
+//ACTION TYPES
+import { GET_ALL_PRODUCTS,
+    GET_DETAIL_PRODUCT,
+    GET_ALL_BRANDS,
+    CLEAN_DETAIL,
+    FILTER_BY_BRAND,
+    FILTER_BY_CATEGORY,
+    SEARCH_PRODUCTS,
+    CREATE_PRODUCT,
+    ORDER_PRODUCTS
+             } from "../actions/types";
 
-
-            return dispatch({
-                type: GET_POKEMONS,
-                payload: data
-            })
-        } catch (error) {
-            console.log(error.message)
-        }
-    }
-}
-
-export const getDetail = (id) => {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios.get(`http://localhost:3001/pokemons/` + id);
-
-            return dispatch({
-                type: GET_DETAIL,
-                payload: data
-            })
-        } catch (error) {
-            console.log(error.message)
-        }
-    }
-}
-
-export const getByName = (nombre) => {
-    return async (dispatch) => {
-        try {
-
-            const { data } = await axios.get(`http://localhost:3001/pokemons?nombre=${nombre}`);
-
-            return dispatch({
-                type: GET_BY_NAME,
-                payload: data
-            });
-        } catch (error) {
-            console.log(error.message)
+    //ACTIONS A LA DB
+    export function getAllProducts(){
+        return async (dispatch) => {
+            try {
+                const infoAPI = await axios.get(`http://localhost:3001/products`);
+                return dispatch(
+                    { 
+                        type: GET_ALL_PRODUCTS, 
+                        payload: infoAPI.data
+                    });
+            } catch (error) {
+                console.log(error.response.data.error);
+            }
         }
     }
 
-}
-
-export const getTypes = () => {
-    return async (dispatch) => {
-        try {
-            const { data } = await axios.get('http://localhost:3001/types/', {});
-            return dispatch({
-                type: GET_TYPES,
-                payload: data
-            });
-        } catch (error) {
-            console.log(error.message)
+    export function getProductsById(id){
+        return async (dispatch) => {
+            try {
+                const infoAPI = (await axios.get(`http://localhost:3001/products/${id}`)).data;
+                return dispatch(
+                    { type: GET_DETAIL_PRODUCT, 
+                        payload: infoAPI
+                    });
+            } catch (error) {
+                console.log(error.response.data.error);
+            }
         }
     }
-}
-export const filterDbApi = (value) => {
+
+    export function getByName(name){
+        return async (dispatch) => {
+            try {
+                const infoAPI = await axios.get(`http://localhost:3001/products/?name=${name}`);
+                return dispatch(
+                    { 
+                        type: SEARCH_PRODUCTS,  
+                        payload: infoAPI.data
+                    });
+            } catch (error) {
+                console.log(error.response.data.error);
+            }
+        }
+    }
+
+    export function postProduct(product){
+        return async (dispatch) => {
+            try {
+                const infoAPI = (await axios.post(`http://localhost:3001/products`, product)).data;
+                alert('Producto creado!')
+                return dispatch(
+                    { 
+                        type: CREATE_PRODUCT,  
+                        payload: infoAPI
+                    });
+            } catch (error) {
+                alert('Faltan datos?')
+                console.log(error.response.data.error);
+            }
+        }
+    }
+
+    export function getAllBrands(brand){
+        return async (dispatch) => {
+            try {
+                const infoAPI = (await axios.get(`http://localhost:3001/?brand=${brand}`)).data;
+                return dispatch(
+                    { 
+                        type: GET_ALL_BRANDS,  
+                        payload: infoAPI
+                    });
+            } catch (error) {
+                console.log(error.response.data.error);
+            }
+        }
+    }
+
+    //ACTIONS DE FILTRADO
+
+    export const cleanDetail = () =>{
+        return {
+            type: CLEAN_DETAIL
+        };
+    }
+
+    export const filterByBrand = (brand) => {
+        return{
+            type: FILTER_BY_BRAND,
+            payload: brand
+        }
+    }
+
+    export const filterByCategory = (source) => {
+        return{
+            type: FILTER_BY_CATEGORY,
+            payload: source
+        }
+    }
     
-    return {
-        type: FILTERDBAPI,
-        payload : value
+
+    export const orderProducts = (order) => {
+        return{
+            type: ORDER_PRODUCTS,
+            payload: order
+        }
     }
-}
-
-export const filterType = (payload) => {
-    return {
-        type: FILTER_TYPE,
-        payload
-    };
-};
-
-export const orderName = (order) =>{
-    return {
-        type:ORDER_NAME,
-        payload: order
-    }
-}
-
-export const orderAttack = (payload) => {
-    return {
-        type: ORDER_ATTACK,
-        payload
-    };
-};
-
-export const cleanDetail = () =>{
-    return {
-        type: CLEAN_DETAIL,
-       
-    }
-}
-
-// export const createPokemon = (info) => {
-//     return async (dispatch) => {
-//         try {
-//             const response = await axios.post('http://localhost:3001/pokemons/', info)
-//             dispatch({
-//                 type: CREATE_POKEMON,
-//                 payload: response.data
-//             });
-//             alert("Pokemon creado con exito");
-//             return response;
-//         } catch (error) {
-//             console.log(error.message)
-//         }
-//     }
-// }
-
