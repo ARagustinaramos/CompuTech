b0b3e91581909a625a163a4f63d2bbf1248537a3
RodriguezSN <gamer.infante51@gmail.com>
2024-05-25 17:49:05 -0300

    agregue /id/:id al userRouter

--- back/src/routes/userRoutes.js
+++ back/src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const getUserHandlers = require("../handlers/userHandlers/getUserHandlers");
 const postUserHandlers = require("../handlers/userHandlers/postUserHandlres"); // Corregido el nombre del archivo
+const getUserByIdHandlers = require("../handlers/userHandlers/getUserByIdHandlers");
 
 const userRouter = Router();
 
 userRouter.get("/", getUserHandlers);
+userRouter.get("/id/:id", getUserByIdHandlers);
 userRouter.post("/", postUserHandlers);
 
 module.exports = userRouter;
