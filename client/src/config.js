b7db508cbbcf4bf3ffd1e3d7bc47b5483619f036
Hernán Ponce de León Aguirre <135001744+HPoncedeLeonAguirre@users.noreply.github.com>
2024-05-25 13:09:53 -0500

    Arreglando el deploy para que cualquiera lo vea

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,7 @@
-const API_URL = process.env.REACT_APP_BACK_URL || 'http://localhost:3001';
+let API_URL = 'https://computechback.onrender.com'; // URL del backend en Render por defecto
+
+if (process.env.NODE_ENV === 'development') {
+    API_URL = process.env.REACT_APP_LOCAL_BACKEND_URL || 'http://localhost:3001';
+}
+
 export default API_URL;
