fd3ce9e2e3dfbc47c2d895e5a237f3f8a80bf732
JGonzalezGarcia10 <jorgecarasa10@gmail.com>
2024-05-26 15:59:26 +0200

    deshago mi paginado

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { Pagination as FlowbitePagination } from 'flowbite-react';
+import React from 'react'
+import styles from './pagination.module.css'
 
-const CustomPagination = ({ currentPage, totalPages, onPageChange }) => {
+ const customPagination = ({ currentPage, setCurrentPage, nPages }) => {
+  const next = () => {
+    if (currentPage !== nPages) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  const prev = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
   return (
-    <FlowbitePagination
-      currentPage={currentPage}
-      totalPages={totalPages}
-      onPageChange={(page) => {
-        onPageChange(page);
-      }}
-      showIcons
-      previousLabel="Anterior"
-      nextLabel="Siguiente"
-    />
-  );
-};
+    <div className={styles.pagination}>
+      <h4 onClick={prev}>Anterior </h4>
+      <div> {currentPage} / {nPages}</div>
+      <h4 onClick={next} >Siguiente</h4>
+    </div>
+  )
+}
 
-export default CustomPagination;
+export default customPagination
--- client/src/views/home/Home.jsx
+++ client/src/views/home/Home.jsx
@@ -1,7 +1,6 @@
-"use client";
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Flowbite } from 'flowbite-react';
+import { Flowbite, Pagination as FlowbitePagination } from 'flowbite-react';
 import CustomPagination from '../../components/pagination/Pagination.jsx';
 
 import Cards from '../../components/cards/Cards';
