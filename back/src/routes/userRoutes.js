44a66ebc53bf5b8416bfe6041633281bfb32a25a
RodriguezSN <gamer.infante51@gmail.com>
2024-05-26 01:05:04 -0300

    ruta /name agregada

--- back/src/routes/userRoutes.js
+++ back/src/routes/userRoutes.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const getUserHandlers = require("../handlers/userHandlers/getUserHandlers");
 const postUserHandlers = require("../handlers/userHandlers/postUserHandlres"); // Corregido el nombre del archivo
 const getUserByIdHandlers = require("../handlers/userHandlers/getUserByIdHandlers");
+const getUserByNameHandlers = require("../handlers/userHandlers/getUserByNameHandlers");
 
 const userRouter = Router();
 
 userRouter.get("/", getUserHandlers);
 userRouter.get("/:id", getUserByIdHandlers);
+userRouter.get("/name", getUserByNameHandlers);
 userRouter.post("/", postUserHandlers);
 
 module.exports = userRouter;
