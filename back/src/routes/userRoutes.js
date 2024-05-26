cd78a1ce4cd5a935ac3b7864f09804547797c6c1
RodriguezSN <gamer.infante51@gmail.com>
2024-05-26 06:15:50 -0300

    añadi la ruta user/email

--- back/src/routes/userRoutes.js
+++ back/src/routes/userRoutes.js
@@ -5,12 +5,14 @@ const getUserByIdHandlers = require("../handlers/userHandlers/getUserByIdHandler
 const getUserByNameHandlers = require("../handlers/userHandlers/getUserByNameHandlers");
 const putUserHandlers = require("../handlers/userHandlers/putUserHandlers");
 const putAdminHandlers = require("../handlers/userHandlers/putAdminHandlers");
+const getUserByEmailHandlers = require("../handlers/userHandlers/getUserByEmailHandlers");
 
 const userRouter = Router();
 
 userRouter.get("/", getUserHandlers);
 userRouter.get("/name", getUserByNameHandlers);
 userRouter.get("/:id", getUserByIdHandlers);
+userRouter.get("/email", getUserByEmailHandlers);
 userRouter.post("/", postUserHandlers);
 userRouter.put("/:id", putUserHandlers);
 userRouter.put("/admin/:id", putAdminHandlers);
