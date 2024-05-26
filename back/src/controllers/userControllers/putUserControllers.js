c2f9f1dc799e53c426388c65ea9fb4496c2ddb86
RodriguezSN <gamer.infante51@gmail.com>
2024-05-26 02:12:26 -0300

    put users funcionando

--- back/src/controllers/userControllers/putUserControllers.js
+++ back/src/controllers/userControllers/putUserControllers.js
@@ -1,2 +1,19 @@
-const putUserControllers = async (id) => {};
+const { User } = require("../../config/db");
+const putUserControllers = async (id, update) => {
+	const user = await User.findByPk(id);
+
+	if (!user) {
+		return "user with Id no found";
+	}
+	if (update.name) user.name = update.name;
+	if (update.email) user.mail = update.email;
+	if (update.phone) user.phone = update.phone;
+	if (update.picture) user.image = update.picture;
+	if (update.address) user.address = update.address;
+	if (update.active) user.active = update.active;
+	if (update.shoppingCart) user.shoppingCart = update.shoppingCart;
+	if (update.recurringPayment) user.recurringPayment = update.recurringPayment;
+
+	user.save();
+};
 module.exports = putUserControllers;
--- back/src/handlers/userHandlers/putUserHandlers.js
+++ back/src/handlers/userHandlers/putUserHandlers.js
@@ -3,7 +3,8 @@ const putUserControllers = require("../../controllers/userControllers/putUserCon
 const putUserHandlers = async (req, res) => {
 	try {
 		const id = req.params.id;
-		const response = await putUserControllers(id);
+		const update = req.body;
+		const response = await putUserControllers(id, update);
 		res.status(200).json(response);
 	} catch (error) {
 		res.status(500).json(error.message);
