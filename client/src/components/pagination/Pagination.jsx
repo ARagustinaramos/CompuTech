ca285f13529b055dbe86d5578aec5cbab82ec7ae
RodriguezSN <gamer.infante51@gmail.com>
2024-05-26 16:41:56 -0300

    import arreglado en pagination

--- client/src/components/pagination/Pagination.jsx
+++ client/src/components/pagination/Pagination.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
-
+import React from "react";
+import styles from "./pagination.module.css";
 const Pagination = ({ currentPage, setCurrentPage, totalPages }) => {
-  const next = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
+	const next = () => {
+		if (currentPage < totalPages) {
+			setCurrentPage(currentPage + 1);
+		}
+	};
 
-  const prev = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
+	const prev = () => {
+		if (currentPage > 1) {
+			setCurrentPage(currentPage - 1);
+		}
+	};
 
-  return (
-    <div className={styles.pagination}>
-      <h4 onClick={prev} style={{ cursor: currentPage > 1 ? 'pointer' : 'default' }}>
-        Anterior
-      </h4>
-      <div>{currentPage} / {totalPages}</div>
-      <h4 onClick={next} style={{ cursor: currentPage < totalPages ? 'pointer' : 'default' }}>
-        Siguiente
-      </h4>
-    </div>
-  );
+	return (
+		<div className={styles.pagination}>
+			<h4
+				onClick={prev}
+				style={{ cursor: currentPage > 1 ? "pointer" : "default" }}
+			>
+				Anterior
+			</h4>
+			<div>
+				{currentPage} / {totalPages}
+			</div>
+			<h4
+				onClick={next}
+				style={{ cursor: currentPage < totalPages ? "pointer" : "default" }}
+			>
+				Siguiente
+			</h4>
+		</div>
+	);
 };
 
 export default Pagination;
