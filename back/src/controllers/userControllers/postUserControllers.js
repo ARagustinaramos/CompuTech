f175055aaddfd084951014ef32da9cff498923d2
RodriguezSN <gamer.infante51@gmail.com>
2024-05-26 22:45:35 -0300

    ahora el users post llega en req.user

--- back/src/controllers/userControllers/postUserControllers.js
+++ back/src/controllers/userControllers/postUserControllers.js
@@ -3,33 +3,33 @@ const { User } = require("../../config/db");
 const postUserControllers = async (body, user) => {
 	// Validar los datos de usuario
 	console.log("user post: ", user);
-	if (!body.name || !body.email || !body.picture) {
-		throw new Error("All fields are required");
-	}
-	// if (!user.name || !user.email || !user.picture) {
+	// if (!body.name || !body.email || !body.picture) {
 	// 	throw new Error("All fields are required");
 	// }
+	if (!user.name || !user.email || !user.picture) {
+		throw new Error("All fields are required");
+	}
 
 	// Verificar si el usuario ya existe
-	const existingUser = await User.findOne({ where: { mail: body.email } });
+	const existingUser = await User.findOne({ where: { mail: user.email } });
 	if (existingUser) {
 		return "User with this email already exists";
 	}
 	// password Polenta22@
 	let rol = false;
-	if (body.email === "eltodopoderoso@gmail.com") {
+	if (user.email === "eltodopoderoso@gmail.com") {
 		rol = true;
 	}
 	// Crear el nuevo usuario
-	let newName = body.name;
-	if (body.name.includes("@")) {
-		const index = body.name.indexOf("@");
-		newName = body.name.substring(0, index);
+	let newName = user.name;
+	if (user.name.includes("@")) {
+		const index = user.name.indexOf("@");
+		newName = user.name.substring(0, index);
 	}
 	const newUser = await User.create({
 		name: newName,
-		mail: body.email,
-		image: body.picture,
+		mail: user.email,
+		image: user.picture,
 		rol: rol,
 		active: true
 	});
