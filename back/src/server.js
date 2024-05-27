8c499fd0f20bf2115a92329f6131af7c9a08abb8
RodriguezSN <gamer.infante51@gmail.com>
2024-05-26 21:04:56 -0300

    configuracion del middleware

--- back/src/server.js
+++ back/src/server.js
@@ -4,6 +4,8 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
@@ -25,6 +27,30 @@ server.use(
 		origin: "*" // Reemplaza esto con el dominio de tu frontend
 	})
 );
+
+// Configuración de JWT y autenticación
+const authConfig = {
+	domain: "dev-y3wtga4i20zjum82.us.auth0.com", // Reemplaza "tu-domino" con tu dominio Auth0
+	audience: "https://dev-y3wtga4i20zjum82.us.auth0.com/api/v2/" // Reemplaza "tuservidor.com" con el dominio de tu servidor
+};
+
+const checkJwt = jwt.expressjwt({
+	// Dynamically provide a signing key based on the kid in the header
+	// and the signing keys provided by the JWKS endpoint.
+	secret: jwksRsa.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+	}),
+
+	// Validate the audience and the issuer.
+	audience: authConfig.audience,
+	issuer: `https://${authConfig.domain}/`,
+	algorithms: ["RS256"]
+});
+
+server.use(checkJwt);
 server.use("/", router);
 
 server.use((err, req, res, next) => {
