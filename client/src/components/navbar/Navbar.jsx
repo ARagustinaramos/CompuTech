6773b7a7f445caa7aabe1055f7e785d23aa735b2
JGonzalezGarcia10 <jorgecarasa10@gmail.com>
2024-05-27 18:39:47 +0200

    para la demo sin doble iniciar sesion

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -143,16 +143,7 @@ export default function Navbar() {
 								)}
 							</>
 						)}
-						{!isAuthenticated && (
-							<li className="content-center">
-								<button
-									onClick={() => loginWithRedirect()}
-									className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent content-center"
-								>
-									Iniciar Sesión
-								</button>
-							</li>
-						)}
+						
 						<li className="content-center">
 							{user?.email === 'eltodopoderoso@gmail.com' ? (
 								<Link
