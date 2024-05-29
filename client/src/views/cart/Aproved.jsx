e3296dbd8548d7242bddd91cc6a7b9ae263edf61
MariaFlorenciaGala <mariaflorenciagala8@gmail.com>
2024-05-29 05:58:33 -0300

    Paypal controllers, handlers

---
+++ client/src/views/cart/Aproved.jsx
@@ -0,0 +1,7 @@
+export default function Aproved (){
+    return (
+        <div>
+            <h1>Gracias por su compra</h1>
+        </div>
+    )
+}
---
+++ client/src/views/cart/Cancel.jsx
@@ -0,0 +1,7 @@
+export default function Cancel (){
+    return (
+        <div>
+            <h1>Lo sentimos, se rechazó el pago</h1>
+        </div>
+    )
+}
