7ce7b6dbf3830c4134d1d540c3c4d3d2f76a59d9
ARagustinaramos <agusramos12@gmail.com>
2024-05-30 19:46:43 -0300

    arreglos finales de firebase

--- back/src/handlers/userHandlers/getIdUserHandler.js
+++ back/src/handlers/userHandlers/getIdUserHandler.js
@@ -1,6 +1,6 @@
-const { getUserById } = require('../../controllers/userController');
+const  getUserById  = require('../../controllers/userControllers/getIdUser');
 
-const getUserByIdHandlers = async (req, res) => {
+const getUserByIdHandler = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await getUserById(id);
@@ -15,4 +15,4 @@ const getUserByIdHandlers = async (req, res) => {
   }
 };
 
-module.exports = getUserByIdHandlers;
+module.exports = getUserByIdHandler;
