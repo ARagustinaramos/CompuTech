fe854648b1d4a1abb5f65795e70fe2520691e7e3
RodriguezSN <gamer.infante51@gmail.com>
2024-05-29 22:17:54 -0300

    ahora el delete elimina o inhabilita el producto

--- back/src/handlers/productHandler/deleteProductsHandler.js
+++ back/src/handlers/productHandler/deleteProductsHandler.js
@@ -3,7 +3,11 @@ const deleteProductsControllers = require("../../controllers/productControllers/
 const deleteProducts = async (req, res) => {
 	try {
 		const idProduct = req.params.id;
-		const result = await deleteProductsControllers(idProduct);
+		const { exterminateProduct } = req.body;
+		const result = await deleteProductsControllers(
+			idProduct,
+			exterminateProduct
+		);
 		res.status(200).json({ message: result });
 	} catch (error) {
 		console.log(error.message);
