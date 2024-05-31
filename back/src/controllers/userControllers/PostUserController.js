4bde0ac2d8fbdcf06a12fff596f38505f1fb4ad0
RodriguezSN <gamer.infante51@gmail.com>
2024-05-31 19:59:50 -0300

    ahora al crear usuario revisa si ya esta guardado en la DB y puede enviar el email cuando se registra

--- back/src/controllers/userControllers/PostUserController.js
+++ back/src/controllers/userControllers/PostUserController.js
@@ -1,14 +1,26 @@
+const { where } = require("sequelize");
 const { User } = require("../../config/db");
-
+const sendCorreo = require("../../config/sendGrid");
+const fs = require("fs");
+const path = require("path");
 const postUserController = async (req, res) => {
 	try {
 		const { name, mail, rol } = req.body;
+		const existeUser = await User.findOne({ where: { mail: mail } });
+		if (existeUser) {
+			return res.status(201).json("Usuario ya registrado");
+		}
 		const newUser = await User.create({
 			mail,
 			name,
 			rol: true
 			// Aquí puedes añadir más campos si es necesario
 		});
+
+		const bienvenidoHtml = fs.readFileSync(
+			path.join(__dirname, "../../config/HtmlCorreos/BienvenidoCompuTech.html")
+		);
+		await sendCorreo(mail, bienvenidoHtml);
 		res.status(201).json(newUser);
 	} catch (error) {
 		console.error("Error creating user:", error);
