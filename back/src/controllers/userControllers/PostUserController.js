d8ce987972a48d27228ca741d9fe907203efe83f
RodriguezSN <gamer.infante51@gmail.com>
2024-05-31 15:48:06 -0300

    require de fs, path y sendCorreo  en PostUserController.js

--- back/src/controllers/userControllers/PostUserController.js
+++ back/src/controllers/userControllers/PostUserController.js
@@ -1,22 +1,24 @@
 const { User } = require("../../config/db");
-//const sendEmail = require("../../config/sendEmail");
+const sendCorreo = require("../../../sendGrid");
+const fs = require("fs");
+const path = require("path");
 
 const postUserController = async (userInfo) => {
-  console.log('User Info:', userInfo);
+	console.log("User Info:", userInfo);
 	const { name, email, phone, image, address, roles } = userInfo;
 
 	const user = await User.create({
 		name,
-    mail: email,
-    phone,
-    image,
-    address,
-    active: true,
-    rol: roles.includes('admin'),
-    shoppingCart: [],
-    recurringPayment: {}
+		mail: email,
+		phone,
+		image,
+		address,
+		active: true,
+		rol: roles.includes("admin"),
+		shoppingCart: [],
+		recurringPayment: {}
 	});
-	//sendEmail(email, name);
+
 	return user;
 };
 
