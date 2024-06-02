ebe6c9b80071c2d67e9440119545531d792078ce
Iara Baudino <iaba.sur@gmail.com>
2024-06-02 20:03:20 -0300

    resuelto los users duplicados

--- back/src/controllers/userControllers/PostUserController.js
+++ back/src/controllers/userControllers/PostUserController.js
@@ -3,6 +3,14 @@ const { User } = require("../../config/db"); // Asegúrate de que este path sea 
 const postUserController = async (req, res) => {
   try {
     const { name, mail, image, rol } = req.body;
+    const existingUser = await User.findOne({ where: { mail } });
+
+    if (existingUser) {
+      // Si el usuario ya existe, simplemente devolvemos el usuario existente
+      return res.status(200).json(existingUser);
+    }
+
+    // Si el usuario no existe, lo creamos
     const newUser = await User.create({
       mail,
       name,
@@ -10,6 +18,7 @@ const postUserController = async (req, res) => {
       rol: true,
       // Aquí puedes añadir más campos si es necesario
     });
+
     res.status(201).json(newUser);
   } catch (error) {
     console.error("Error creating user:", error);
