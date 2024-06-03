50e79d8d3e9cb883691c29db84b04881bd8ede28
Laura Camila Tiusabá Gómez <108424409+LauraCamila99@users.noreply.github.com>
2024-06-02 22:22:08 -0500

    LISTO TODO EL CRUD DE ORDEN

--- back/src/routes/orderRoutes.js
+++ back/src/routes/orderRoutes.js
@@ -1,8 +1,15 @@
 const { Router } = require("express");
-const { createOrder } = require('../controllers/orderControllers/CreateorderController'); 
+const createOrderHandler = require('../handlers/orderHandler/createOrderHandler'); 
+const getAllOrdersHandler = require('../handlers/orderHandler/getAllOrderHandler'); 
+const getOrderByIdHandler = require('../handlers/orderHandler/getOrderByIdHandler');
+const getUserOrdersHandler = require('../handlers/orderHandler/getUserOrdersHandler');
+const deleteOrderHandler= require('../handlers/orderHandler/deleteOrderHandler');
 
 const orderRouter = Router();
 
-orderRouter.post('/create-order', createOrder);
-
+orderRouter.get("/",getAllOrdersHandler)
+orderRouter.post("/create-order/:id", createOrderHandler);
+orderRouter.get('/:orderId', getOrderByIdHandler);
+orderRouter.get('/:userId/orders', getUserOrdersHandler);
+orderRouter.delete('/:orderId', deleteOrderHandler);
 module.exports = orderRouter;
