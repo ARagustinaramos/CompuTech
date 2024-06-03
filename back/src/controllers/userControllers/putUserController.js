611fad6adbd5546027b29e1eeb5d684bea3c162c
RodriguezSN <gamer.infante51@gmail.com>
2024-06-03 20:08:07 -0300

    ahora el userData esta desectructurado de req.body

--- back/src/controllers/userControllers/putUserController.js
+++ back/src/controllers/userControllers/putUserController.js
@@ -1,32 +1,32 @@
 const { User } = require("../../config/db");
 
 const putUserController = async (id, userData, res) => {
-  try {
-    // Buscar el usuario por su ID
-    const user = await User.findByPk(id);
+	try {
+		// Buscar el usuario por su ID
+		const user = await User.findByPk(id);
 
-    // Verificar si el usuario existe
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+		// Verificar si el usuario existe
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
 
-    // Actualizar los campos del usuario con los datos proporcionados
-    user.name = userData.name || user.name;
-    user.phone = userData.phone || user.phone;
-    user.image = userData.image || user.image;
-    user.address = userData.address || user.address;
-    user.active = userData.active || user.active;
-    user.shoppingCart = userData.shoppingCart || user.shoppingCart;
-    user.recurringPayment = userData.recurringPayment || user.recurringPayment;
+		// Actualizar los campos del usuario con los datos proporcionados
+		user.name = userData.name || user.name;
+		user.phone = userData.phone || user.phone;
+		user.image = userData.image || user.image;
+		user.address = userData.address || user.address;
+		user.active = userData.active || user.active;
+		user.shoppingCart = userData.shoppingCart || user.shoppingCart;
+		user.recurringPayment = userData.recurringPayment || user.recurringPayment;
 
-    // Guardar los cambios en la base de datos
-    await user.save();
+		// Guardar los cambios en la base de datos
+		await user.save();
 
-    res.status(200).json(user);
-  } catch (error) {
-    console.error("Error updating user:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
+		res.status(200).json(user);
+	} catch (error) {
+		console.error("Error updating user:", error);
+		res.status(500).json({ message: "Internal server error" });
+	}
 };
 
 module.exports = putUserController;
