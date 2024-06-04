6dfbb9f53a801a21563ed7075b578375d3932fa3
Gilberto Díaz <diazp.gilberto@gmail.com>
2024-06-03 21:43:41 -0500

    renderizando reviews en la tabla del Dashboard

--- client/src/components/tables/tableUser/TableDashboardReviews.jsx
+++ client/src/components/tables/tableUser/TableDashboardReviews.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Select, SelectItem } from '@tremor/react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getUsers,getAllSales } from '../../../redux/actions/actions';
+import { getUsers, getAllSales, getDetail } from '../../../redux/actions/actions';
 
 export function TableDashboardReviews() {
   const dispatch = useDispatch();
   const allUsers = useSelector((state) => state.allUsers);
+  const producto = useSelector((state) => state.productDetail);
+  console.log(producto)
 
   // Estado local para manejar el estado activo/inactivo de cada usuario
   const [userStatus, setUserStatus] = useState({});
+
 
   useEffect(() => {
     dispatch(getAllSales());
@@ -29,24 +32,19 @@ export function TableDashboardReviews() {
     }));
   };
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">  
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="p-4">
-              <div className="flex items-center">
-                <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
-              </div>
-            </th>
+
             <th scope="col" className="px-6 py-3">
-              ORDEN
+              USUARIO
             </th>
             <th scope="col" className="px-6 py-3">
               Review
             </th>
             <th scope="col" className="px-6 py-3">
-              Status
+
             </th>
             <th scope="col" className="px-6 py-3">
               Acción
@@ -56,12 +54,6 @@ export function TableDashboardReviews() {
         <tbody>
           {allUsers.map((user) => (
             <tr key={user.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <td className="w-4 p-4">
-                <div className="flex items-center">
-                  <input id={`checkbox-table-search-${user.id}`} type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                  <label htmlFor={`checkbox-table-search-${user.id}`} className="sr-only">checkbox</label>
-                </div>
-              </td>
               <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
                 <img className="w-10 h-10 rounded-full" src={user.image} alt={`${user.name} image`} />
                 <div className="ps-3">
@@ -70,18 +62,12 @@ export function TableDashboardReviews() {
                 </div>
               </th>
               <td className="px-6 py-4">
-                <Select defaultValue="1">
-                  <SelectItem value="1">Usuario</SelectItem>
-                  <SelectItem value="2">Administrador</SelectItem>
-                </Select>
+
               </td>
               <td className="px-6 py-4">
                 <div className="flex items-center">
-                  <div className={`h-2.5 w-2.5 rounded-full ${userStatus[user.id] === 'activo' ? 'bg-green-500' : 'bg-red-500'} me-2`}></div>
-                  <Select defaultValue="1">
-                  <SelectItem value="1">Usuario</SelectItem>
-                  <SelectItem value="2">Administrador</SelectItem>
-                </Select>
+
+
                 </div>
               </td>
               <td className="px-6 py-4">
