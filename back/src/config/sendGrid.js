0cf6fe48c76c67e0eaef1ec757032953fc9e5ddc
RodriguezSN <gamer.infante51@gmail.com>
2024-06-05 23:19:03 -0300

    se agrego compra.html sendgrid modificacion putUserController

--- back/src/config/sendGrid.js
+++ back/src/config/sendGrid.js
@@ -5,7 +5,7 @@ const myCorreo = process.env.EMAIL_USER;
 
 const sendCorreo = async (user, html) => {
 	const msg = {
-		to: user, // Change to your recipient
+		to: `${user}`, // Change to your recipient
 		from: `${myCorreo}`, // Change to your verified sender
 		subject: "Equipo de Computech",
 		text: "and easy to do anywhere, even with Node.js",
--- back/src/controllers/userControllers/putUserController.js
+++ back/src/controllers/userControllers/putUserController.js
@@ -14,41 +14,38 @@ const putUserController = async (id, userData, res) => {
 			return res.status(404).json({ message: "User not found" });
 		}
 
-		// Variables para verificar cambios en shoppingCart y recurringPayment
-		let shoppingCartChanged = false;
-		let recurringPaymentChanged = false;
-
 		// Actualizar los campos del usuario con los datos proporcionados
 		user.name = userData.name || user.name;
 		user.phone = userData.phone || user.phone;
 		user.image = userData.image || user.image;
 		user.address = userData.address || user.address;
 		user.active = userData.active || user.active;
-
-		if (userData.shoppingCart !== undefined && userData.shoppingCart !== user.shoppingCart) {
-			user.shoppingCart = userData.shoppingCart;
-			shoppingCartChanged = true;
-		}
-
-		if (userData.recurringPayment !== undefined && userData.recurringPayment !== user.recurringPayment) {
-			user.recurringPayment = userData.recurringPayment;
-			recurringPaymentChanged = true;
-		}
+		user.shoppingCart = userData.shoppingCart || user.shoppingCart;
+		user.recurringPayment = userData.recurringPayment || user.recurringPayment;
 
 		// Guardar los cambios en la base de datos
 		await user.save();
+		const correoUser = user.mail;
+		const compraRealizada = userData.shoppingCart;
 
-		// Enviar el correo solo si se modificaron shoppingCart o recurringPayment
-		if (shoppingCartChanged || recurringPaymentChanged) {
-			// Busca el archivo html de update
+		if (compraRealizada) {
+			// Leer el archivo HTML de compra
+			const compraHtml = fs.readFileSync(
+				path.join(__dirname, "../../config/HtmlCorreos/Compra.html"),
+				"utf-8"
+			);
+
+			// Enviar correo al usuario después de realizar la compra
+			await sendCorreo(correoUser, compraHtml);
+		} else {
+			// Leer el archivo HTML de actualización
 			const updateHtml = fs.readFileSync(
 				path.join(__dirname, "../../config/HtmlCorreos/Update.html"),
 				"utf-8"
 			);
 
-			// Enviar el correo al usuario que actualizó su perfil
-			const email = user.mail;
-			await sendCorreo(email, updateHtml);
+			// Enviar correo al usuario que actualizó su perfil
+			await sendCorreo(correoUser, updateHtml);
 		}
 
 		res.status(200).json(user);
